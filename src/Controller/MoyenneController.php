6ef7b27fdfb1085c62d1f06e775b8ed47f2aec08
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2021-06-10 02:29:14 +0200

    ctr

--- src/Controller/MoyenneController.php
+++ src/Controller/MoyenneController.php
@@ -9,9 +9,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoyenneController extends AbstractController
 {
     #[Route('/moyenne', name: 'moyenne')]
-    public function index(): Response
+    public function displayMoyenne(): Response
     {
         return $this->render('Admin/les moyennes.html.twig', [
+            'controller_name' => 'MoyenneController',
+        ]);
+    }
+
+
+    #[Route('/addMoyenne', name: 'addMoyenne')]
+    public function addMoyenne(): Response
+    {
+        return $this->render('Admin/ajoutMoyenne.html.twig', [
             'controller_name' => 'MoyenneController',
         ]);
     }
--- src/Controller/ParentController.php
+++ src/Controller/ParentController.php
@@ -9,9 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class ParentController extends AbstractController
 {
     #[Route('/parent', name: 'parent')]
-    public function index(): Response
+    public function displayParent(): Response
     {
         return $this->render('Admin/les parents.html.twig', [
+            'controller_name' => 'ParentController',
+        ]);
+    }
+
+    #[Route('/addParent', name: 'addParent')]
+    public function addParent(): Response
+    {
+        return $this->render('Admin/ajoutParent.html.twig', [
             'controller_name' => 'ParentController',
         ]);
     }
